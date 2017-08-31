d59adc5359b38945a0e049068c65fef28fe53e8d
drkarthik1996 <31504909+drkarthik1996@users.noreply.github.com>
2017-08-31 15:33:59 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main( String[] args ) {
-        System.out.println( "Hello branch1" );
+        System.out.println( "Hello Karthik" );
         System.exit( 0 ); //success
     }
 }
