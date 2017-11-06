0089db16cd348cb30c9ac2a0c320d6360f5d0c9d
drkarthik1996 <31504909+drkarthik1996@users.noreply.github.com>
2017-11-06 14:16:40 +0530

    Update c_program1.c

--- c_program1.c
+++ c_program1.c
@@ -4,8 +4,8 @@ int main()
     int i, j, rows;
 
     printf("Enter number of rows: ");
-    //scanf("%d",&rows);
-    rows = 9;
+    scanf("%d",&rows);
+    //rows = 9;
     for(i=1; i<=rows; ++i)
     {
         for(j=1; j<=i; ++j)
