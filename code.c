702ac0002928cece526857d881ce843bb8df965c
drkarthik1996 <31504909+drkarthik1996@users.noreply.github.com>
2017-11-06 17:10:31 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
    // printf() displays the string inside quotation
    printf("...................Hello Karthik..................");
+   system("Pause");
    return 0;
 }
