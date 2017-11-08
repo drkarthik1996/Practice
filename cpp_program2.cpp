89d99829de8e231e85477a133fee71761b296a88
drkarthik1996 <31504909+drkarthik1996@users.noreply.github.com>
2017-11-08 12:34:16 +0530

    Add files via upload

---
+++ cpp_program2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int rows;
+	
+    rows = atoi(argv[1]);
+    cout << "Number of rows: " << rows << endl;
+
+    for(int i = 1; i <= rows; ++i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    //system("Pause");
+    return 0;
+}
