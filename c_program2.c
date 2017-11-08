0846d8c60de0b2214e10c88e4d5e659daedabe95
drkarthik1996 <31504909+drkarthik1996@users.noreply.github.com>
2017-11-08 12:45:56 +0530

    Update c_program2.c

--- c_program2.c
+++ c_program2.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
     int i, j, rows;
-	
+    cout << "\n-------C++ Program with Commandline Arguments--------\n" << endl;
     rows = atoi(argv[1]);
     printf("Number of rows : %d\n",rows);
 
